d1c44da423f0f95c6036b71259940dcd37a36768
MITE0303 <dice010303@gmail.com>
2023-10-10 21:36:02 +0900

    1010

--- 실습 2. 장바구니/main.js
+++ 실습 2. 장바구니/main.js
@@ -9,7 +9,11 @@ function addToCart(event) {
     let product = event.target.parentElement;
     let name = product.querySelector("span").textContent;
     let priceText = product.querySelector(".price").textContent; //문자열을 숫자로 반환
-    let price = parseInt(priceText.slice(0, -1));
+    let price = parseInt(priceText.slice(0, -1)); //"원" 뺴기
+    // 문자열 자르기 - 뒤에서부터 (시작위치 , 종료 위치 - 1 )
+    // 0  1  2  3  4  5
+    // 자 바 스 크 립 트
+    // -6 -5 -4 -3 -2 -1
     let quantity = parseInt(product.querySelector(".quantity").value);
 
     if (quantity > 0) {
